c7aa7c9a181454e49df4505b6d852f57d9c84bcb
vision0101 <shimcy2010@gmail.com>
2021-11-18 15:40:36 +0900

    [FEAT] find view linear card 생성

--- app/src/main/java/org/sopt/seminar_6_android/ui/find/FindFragment.kt
+++ app/src/main/java/org/sopt/seminar_6_android/ui/find/FindFragment.kt
@@ -10,6 +10,9 @@ import org.sopt.seminar_6_android.databinding.FragmentFindBinding
 class FindFragment : Fragment() {
     private var _binding: FragmentFindBinding? = null
     private val binding get() = _binding ?: error("binding이 초기화 되지 않았습니다.")
+    private lateinit var newsCardAdapter: NewsCardAdapter
+    private lateinit var newsLinearCardAdapter: NewsLinearCardAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +20,40 @@ class FindFragment : Fragment() {
     ): View? {
         _binding = FragmentFindBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+        initGridAdapter()
+        initLinearAdapter()
+        return binding.root
+    }
+
+    private fun initGridAdapter() {
+        newsCardAdapter = NewsCardAdapter()
+        binding.includeNewsCard.rvNewsCard.adapter = newsCardAdapter
+
+        newsCardAdapter.newsList.addAll(
+            listOf(
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1"),
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1"),
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1")
+            )
+        )
+        newsCardAdapter.notifyDataSetChanged()
+        Log.d("data되고있니?","${newsCardAdapter.newsList.toString()}data 들어가고있니")
+    }
+
+    private fun initLinearAdapter() {
+        Log.d("init되고있니?","initAdapter 호출")
+        newsLinearCardAdapter = NewsLinearCardAdapter()
+        binding.includeNewsLinearCard.rvNewsCard.adapter = newsLinearCardAdapter
+
+        newsLinearCardAdapter.newsList.addAll(
+            listOf(
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1"),
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1"),
+                NewsGridData("https://cdn.pixabay.com/photo/2021/11/08/11/19/buildings-6778915_1280.jpg", "설명1")
+            )
+        )
+        newsLinearCardAdapter.notifyDataSetChanged()
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/org/sopt/seminar_6_android/ui/find/NewsLinearCardAdapter.kt
@@ -0,0 +1,36 @@
+package org.sopt.seminar_6_android.ui.find
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.sopt.seminar_6_android.databinding.ItemFindNewsLinearListBinding
+
+class NewsLinearCardAdapter : RecyclerView.Adapter<NewsLinearCardAdapter.CardViewHolder>() {
+    val newsList = mutableListOf<NewsGridData>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CardViewHolder {
+        val binding = ItemFindNewsLinearListBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false
+        )
+        return CardViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        holder.onBind(newsList[position])
+    }
+
+    override fun getItemCount(): Int = newsList.size
+
+    class CardViewHolder(private val binding: ItemFindNewsLinearListBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: NewsGridData) {
+            Glide.with(itemView).load(data.newsImg).into(binding.ivLinearNewsImg)
+            binding.tvLinearNewsTitle.text = data.newsTitle
+        }
+    }
+}
