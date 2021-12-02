28ba86f53e69ba54464ca5e61cf1a41de093b219
Grandelwald <artjr97@naver.com>
2021-12-03 01:54:22 +0900

    [CHORE] - kakaotvservice 파일명 변경

--- app/src/main/java/org/sopt/seminar_6_android/data/api/KakaoTVService.kt
+++ app/src/main/java/org/sopt/seminar_6_android/data/api/KakaoTVService.kt
@@ -2,6 +2,6 @@ package org.sopt.seminar_6_android.data.api
 
 import retrofit2.http.Headers
 
-interface KakaotvService {
-    @Headers("Content")
+interface KakaoTVService {
+    @Headers("Content-Type:application/json")
 }
